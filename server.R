a105c5ef36fe1f69f1a140d7314b4803071cd326
hhk123g <hhk123@mail.ustc.edu.cn>
2016-09-29 13:28:51 +0800

    Add files via upload

---
+++ server.R
@@ -0,0 +1,13 @@
+shinyServer(
+  function(input, output) {
+  
+  output$text1 <- renderText({
+    paste("You have selected this",input$var)
+  })
+  
+  output$text2 <- renderText({
+    paste("You have selected that goes from",
+    input$range[1],"to",input$range[2])
+  })
+  }
+)
